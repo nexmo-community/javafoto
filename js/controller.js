c53bb9946d88c7e181921388340e22fdd91fda41
Tim Lytle <tim@timlytle.net>
2015-10-26 01:12:01 -0400

    Update controller.js
    Fixed path.

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,7 @@
 var phonecatApp = angular.module('fotoApp', []);
 
 phonecatApp.controller('StripCtrl', function ($scope, $http) {
-    $http.get('/data.json').success(function(data){
+    $http.get('data.json').success(function(data){
         $scope.sessions = [];
         angular.forEach(data.sessions, function(value){
            $scope.sessions.push(value);
